892b585bd7f8a32a39f7d6bf34b412ab92f88515
Onesso <odhiambofrank1965@gmail.com>
2025-08-25 14:26:46 +0300

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     CommonModule,
     MatRippleModule,
     FormsModule,
     ReactiveFormsModule,
     MatIconModule,
+    NgbCarouselModule,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -21,5 +30,25 @@ export class App {
 
   public togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
+  }
+
+  images: string[] = ['image2.png', 'image3.png', 'image.png'];
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+  });
+
+  submitLoginForm() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+      console.log('Form is invalid');
+    }
   }
 }
